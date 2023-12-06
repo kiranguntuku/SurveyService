49d2bd522a49f9435d447e2ec3b171d9b89754d6
unknown <kiranguntuku@gmail.com>
2023-12-07 01:29:13 +0530

    Added sys outs

--- src/main/java/com/kiran/survey/SurveyApplication.java
+++ src/main/java/com/kiran/survey/SurveyApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SurveyApplication {
 
 	public static void main(String[] args) {
+		System.out.println("App Starting");
 		SpringApplication.run(SurveyApplication.class, args);
+		System.out.println("App Started");
 	}
 
 }
